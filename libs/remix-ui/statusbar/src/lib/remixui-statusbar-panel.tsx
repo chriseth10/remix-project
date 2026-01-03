71aeaf021522d878b8271658f43d79debff2ace5
mempoolv4 <144087625+mempoolv4@users.noreply.github.com>
2026-01-03 20:52:53 +1100

    Update remixui-statusbar-panel.tsx

--- libs/remix-ui/statusbar/src/lib/remixui-statusbar-panel.tsx
+++ libs/remix-ui/statusbar/src/lib/remixui-statusbar-panel.tsx
@@ -75,51 +75,5 @@ export function RemixUIStatusBar({ statusBarPlugin }: RemixUIStatusBarProps) {
       <DesktopStatus /></div></>)
   }
 
-  return (
-    <>
-      <StatusBarContextProvider>
-        {(platform !== appPlatformTypes.desktop) && showScamDetails && (
-          <FloatingFocusManager context={context} modal={false}>
-            <ScamDetails
-              refs={refs}
-              floatStyle={{
-                ...floatingStyles,
-                minHeight: 'auto',
-                alignContent: 'center',
-                paddingRight: '0.5rem',
-                bottom: '-8,5rem',
-                left: '0rem'
-              }}
-              getFloatingProps={getFloatingProps}
-              scamAlerts={scamAlerts}
-            />
-          </FloatingFocusManager>
-        )}
-        <div className="d-flex remixui_statusbar_height flex-row bg-info justify-content-between align-items-center">
-          {(platform !== appPlatformTypes.desktop) && <div className="remixui_statusbar bg-warning px-2 remixui_statusbar_custom_padding d-flex justify-center align-items-center">
-            <ScamAlertStatus refs={refs} getReferenceProps={getReferenceProps} />
-          </div>}
-          {(platform === appPlatformTypes.desktop) && <div className="remixui_statusbar">
-            <DesktopStatus /></div>}
-
-          <div className='d-flex w-100 justify-content-between'>
-            <div className="remixui_statusbar remixui_statusbar_gitstatus">
-              <GitStatus plugin={statusBarPlugin} gitBranchName={gitBranchName} setGitBranchName={setGitBranchName} />
-            </div>
-          </div>
-
-          <div className="w-100 remixui_statusbar">
-            <DidYouKnow />
-          </div>
-
-          <div className="remixui_statusbar d-flex w-100 justify-content-end p-0">
-
-            <div className="remixui_statusbar">
-              <AIStatus plugin={statusBarPlugin} aiActive={lightAiUp} isAiActive={isAiActive} setIsAiActive={setIsAiActive} />
-            </div>
-          </div>
-        </div>
-      </StatusBarContextProvider>
-    </>
-  )
+    return null
 }
